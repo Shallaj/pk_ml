3fe69daef69838ae7d14fed7f43ebe7a5c0b7a14
Shallaj <Shahin.hallaj@gmail.com>
2023-12-14 14:55:48 -0800

    no message

--- All of Us _ GLP_1R_Agonists/utils/condition_parser.py
+++ All of Us _ GLP_1R_Agonists/utils/condition_parser.py
@@ -32,16 +32,11 @@ import pandas as pd
 
 def condition_parser(condition_df, my_dataframe, condition_name):
     merged_df = pd.merge(condition_df, my_dataframe, on='person_id', how='left')
-    print(merged_df)
-
     merged_df['final_index_date'] = pd.to_datetime(merged_df['final_index_date'], format='%m %d %Y').dt.tz_localize(None)
     merged_df['condition_start_datetime'] = pd.to_datetime(merged_df['condition_start_datetime'], format='%m %d %Y').dt.tz_localize(None)
-
     merged_df = merged_df[merged_df['condition_start_datetime'] < merged_df['final_index_date']]
     merged_df = merged_df.drop_duplicates(subset='person_id', keep='first')
-
-    print(merged_df)
-
+    
     tidy_condition_name = pd.DataFrame(columns = ['person_id', condition_name])
     tidy_condition_name['person_id'] = merged_df['person_id']
     tidy_condition_name[condition_name] = 1
